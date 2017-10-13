381b4dfa71298803dff50ccd24de6678932f29db
LAPTOP-3DUPLS01\Lenovo <leedongsu97@gmail.com>
2017-10-13 17:01:26 +0900

    doing homework

--- 2016182026/main_state.py
+++ 2016182026/main_state.py
@@ -1,6 +1,6 @@
 from pico2d import *
 import os
-os.chdir('c:\\Users\\lee\Desktop\\2D')
+os.chdir('c:\\Users\\Lenovo\\Desktop\\2D')
 import title_state
 import game_framework
 import random
@@ -9,41 +9,42 @@ class Boy:
     LEFT_RUN,RIGHT_RUN,LEFT_STAND,RIGHT_STAND=0,1,2,3
     def __init__(self):
         self.x,self.y=random.randint(100,700),90
-        self.frame=0
+        self.frame=random.randint(0,7)
         self.dir=1
         self.run_frames=0
         self.stand_frames=0
-        self.state=self.RIGHT_RUN
+        self.state=random.randint(2,3)
         if Boy.image==None:
             Boy.image=load_image('animation_sheet.png')
     def handle_left_run(self):
         self.x-=5
         self.run_frames+=1
         if self.x<0:
-            self.state=self.RIGHT_RUN
             self.x=0
-        if self.run_frames==100:
-            self.state=self.LEFT_STAND
-            self.stand_frames=0
     def handle_left_stand(self):
         self.stand_frames+=1
-        if self.stand_frames==50:
-            self.state=self.LEFT_RUN
-            self.run_frames=0
     def handle_right_run(self):
         self.x+=5
         self.run_frames+=1
         if self.x>800:
-            self.state=self.LEFT_RUN
             self.x=800
-        if self.run_frames==100:
-            self.state=self.RIGHT_STAND
-            self.stand_frames=0
     def handle_right_stand(self):
         self.stand_frames+=1
-        if self.stand_frames==50:
-            self.state=self.RIGHT_RUN
-            self.run_frames=0
+    def handle_event(self,event):
+        if (event.type,event.key)==(SDL_KEYDOWN,SDLK_LEFT):
+            if self.state in(self.RIGHT_STAND, self.LEFT_STAND):
+                self.state=self.LEFT_RUN
+        elif (event.type,event.key)==(SDL_KEYDOWN,SDLK_RIGHT):
+            if self.state in (self.RIGHT_STAND,self.LEFT_STAND):
+                self.state=self.RIGHT_RUN
+        elif (event.type,event.key)==(SDL_KEYUP,SDLK_LEFT):
+            if self.state in(self.LEFT_RUN,):
+                self.state=self.LEFT_STAND
+                self.stand_frames=0
+        elif (event.type,event.key)==(SDL_KEYUP,SDLK_RIGHT):
+            if self.state in (self.RIGHT_RUN,):
+                self.state=self.RIGHT_STAND
+                self.stand_frames=0
     handle_state={
             LEFT_RUN:handle_left_run,
             RIGHT_RUN:handle_right_run,
@@ -53,6 +54,10 @@ class Boy:
     def update(self):
             self.frame=(self.frame+1)%8
             self.handle_state[self.state](self)
+            if self.state==self.RIGHT_RUN:
+                self.x=min(800,self.x+5)
+            elif self.state==self.LEFT_RUN:
+                self.x=max(0,self.x-5)
 
     def draw(self):
         self.image.clip_draw(self.frame*100,self.state*100,100,100,self.x,self.y)
@@ -84,6 +89,7 @@ def handle_events():
  global running
  global z
  global s
+ global team
  events=get_events()
  for event in events:
   if event.type==SDL_QUIT:
@@ -99,14 +105,14 @@ def handle_events():
            if z<0:
                z=0
            s=str(z)
-       elif event.key==SDLK_RIGHT:
-            team[z].x+=10
-       elif event.key==SDLK_LEFT:
-            team[z].x-=10
        elif event.key==SDLK_ESCAPE:
             game_framework.change_state(title_state)
-  elif event.type==SDL_MOUSEMOTION:
-   team[z].x,team[z].y=event.x,600-event.y
+       else :
+            team[z].handle_event(event)
+  elif event.type==SDL_KEYUP:
+      team[z].handle_event(event)
+  elif event.type==SDL_MOUSEMOTION: 
+       team[z].x,team[z].y=event.x,600-event.y
 
 def update():
    
